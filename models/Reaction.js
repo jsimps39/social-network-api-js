53037b2e4e2a1bb049fc0e38ef173082ba0d3e24
James S <jamests1000@gmail.com>
2023-02-06 20:44:11 -0500

    updated Reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -6,13 +6,18 @@ const reactionSchema = new mongoose.Schema({
   // Configure individual properties using Schema Types
   reactionId: { type: Number, default: () => new Types.ObjectId()},
   reactionBody: { type: String, required: true, max_length: 280},
-  username: String,
+  username: {type: String, required: true},
   // Use built in date method to get current date
   createdAt: { type: Date, default: Date.now }, //use a getter method to format the timestamp on query
+},
+{
+    toJSON: {
+      virtuals: true,
+    }
 });
 
 // Using mongoose.model() to compile a model based on the schema 'bookSchema'
-const Reaction = mongoose.model('MyReaction', reactionSchema);
+//const Reaction = mongoose.model('MyReaction', reactionSchema);
 
 // // Create a new instance of the model, a document
 // Book.create([
@@ -25,4 +30,4 @@ const Reaction = mongoose.model('MyReaction', reactionSchema);
 //   { title: 'Hello World Infinity and Beyond', price: 4, inStock: true },
 // ]);
 
-module.exports = Reaction;
+module.exports = Reaction; //or reactionSchema?
